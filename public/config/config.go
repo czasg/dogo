a81bc9159a51e4b77feed1701aac5653259c13ea
cza <44974561+czasg@users.noreply.github.com>
2024-01-30 02:17:52 +0000

    feat: update

--- public/config/config.go
+++ public/config/config.go
@@ -3,14 +3,24 @@ package config
 var cfg *Config
 
 type Config struct {
-	Http HttpConfig
+	Http  HttpConfig  `env:"HTTP"`
+	Redis RedisConfig `env:"REDIS"`
 }
 
 type HttpConfig struct {
-	Port         int `env:",default=8080"`
-	GraceTimeout int `env:",default=5"`
-	ReadTimeout  int `env:",default=0"`
-	WriteTimeout int `env:",default=0"`
+	Port         int `env:"PORT,default=8080"`
+	GraceTimeout int `env:"GRACE_TIMEOUT,default=5"`
+	ReadTimeout  int `env:"READ_TIMEOUT,default=0"`
+	WriteTimeout int `env:"WRITE_TIMEOUT,default=0"`
+}
+
+type RedisConfig struct {
+	Address     string `env:"ADDRESS"`
+	Password    string `env:"PASSWORD"`
+	DB          int    `env:"DB,default=0"`
+	PoolSize    int    `env:"POOL_SIZE,default=3"`
+	MaxRetries  int    `env:"MAX_RETRIES,default=1"`
+	MinIdleSize int    `env:"MIN_IDLE_SIZE,default=1"`
 }
 
 func GetConfig() *Config {
